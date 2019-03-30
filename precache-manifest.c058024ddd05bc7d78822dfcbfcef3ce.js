a8287d2f4920a123b6f9edca2d9d88186cb04592
badraih <aaa@aAas-MacBook-Pro-3.local>
2019-03-30 11:50:42 +0300

    Updates

---
+++ precache-manifest.c058024ddd05bc7d78822dfcbfcef3ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "243f976bca31714ed0b2",
+    "url": "/static/css/main.39e42ba2.chunk.css"
+  },
+  {
+    "revision": "243f976bca31714ed0b2",
+    "url": "/static/js/main.4630ccc3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d8f025716f7ca61691e7",
+    "url": "/static/css/2.535b08be.chunk.css"
+  },
+  {
+    "revision": "d8f025716f7ca61691e7",
+    "url": "/static/js/2.a94d1e15.chunk.js"
+  },
+  {
+    "revision": "8c877b7622cadf9b5701fc318360a4df",
+    "url": "/index.html"
+  }
+];
