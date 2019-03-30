83bc2d45756ee5a509dcb2e0f8e01df4055a748a
badraih <aaa@aAas-MacBook-Pro-3.local>
2019-03-30 11:59:04 +0300

    Updates

---
+++ precache-manifest.4b9dc536cdab258127400e78da47e60b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03780191fdf99d41b19d",
+    "url": "/background_design/static/css/main.39e42ba2.chunk.css"
+  },
+  {
+    "revision": "03780191fdf99d41b19d",
+    "url": "/background_design/static/js/main.02e53006.chunk.js"
+  },
+  {
+    "revision": "54c1f33f5fba11bb8141",
+    "url": "/background_design/static/js/runtime~main.c3eb0558.js"
+  },
+  {
+    "revision": "d8f025716f7ca61691e7",
+    "url": "/background_design/static/css/2.535b08be.chunk.css"
+  },
+  {
+    "revision": "d8f025716f7ca61691e7",
+    "url": "/background_design/static/js/2.a94d1e15.chunk.js"
+  },
+  {
+    "revision": "dbaec09949ffdb2782739272d347ee5f",
+    "url": "/background_design/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c058024ddd05bc7d78822dfcbfcef3ce.js"
+  "/background_design/precache-manifest.4b9dc536cdab258127400e78da47e60b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/background_design/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
